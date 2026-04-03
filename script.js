4066899f53c1b0947bbefaf169a26be1f74e5364
Aditya Nandkhile <92627743+nandkhile1111@users.noreply.github.com>
2026-04-03 13:07:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 <script>
 function changeText() {
-    const messages = [
-        "CI/CD is awesome 🚀",
-        "Automation saves time!",
-        "Deploy faster with CI/CD 🔥"
-    ];
-
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    document.getElementById("message").innerText = messages[randomIndex];
+    document.getElementById("message").innerText = "Working!";
 }
 </script>
